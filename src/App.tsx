5951c34ee6b1c39e59d565b9c92a2ff9323e45ea
lucienzera <lucienzera26@gmail.com>
2024-01-21 22:48:49 +0800

    add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { create } from 'zustand';
 import './App.css';
 
+interface BearState {
+  bears: number;
+  increase: (by: number) => void;
+}
+
+const useBearStore = create<BearState>()((set) => ({
+  bears: 0,
+  increase: (by) => set((state) => ({ bears: state.bears + by })),
+}));
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const bears = useBearStore((state) => state.bears);
+  const increase = useBearStore((state) => state.increase);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">{bears} around here ...</h1>
+      <button onClick={() => increase(2)}>press to increase count</button>
+    </div>
+  );
 }
 
 export default App;
