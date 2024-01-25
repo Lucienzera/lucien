f13b7c09b6262ec0fc4772051277eb0d3faa16e5
lucienzera <lucienzera26@gmail.com>
2024-01-25 12:41:29 +0800

    replace tailwind and preline with styleX

--- src/route/AdminRoute.tsx
+++ src/route/AdminRoute.tsx
@@ -1,3 +1,14 @@
+import stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  container: {
+    backgroundColor: 'red',
+    borderRadius: 8,
+    padding: 16,
+    boxShadow: '0 0 16px rgba(0, 0, 0, 0.1)',
+  },
+});
+
 export default function AdminRoute() {
   /*TODO: 
       This section should include checking for user cookies:
@@ -9,8 +20,8 @@ export default function AdminRoute() {
       if cookie does not exists then redirect to /login
       */
   return (
-    <>
+    <div {...stylex.props(styles.container)}>
       <h1>In admin route now</h1>
-    </>
+    </div>
   );
 }
