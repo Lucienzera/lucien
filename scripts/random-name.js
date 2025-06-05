97a04ef93aaad8052f95d557b2f0e4e521212eff
lucienzera26 <lucienzera26@gmail.com>
2025-06-05 15:52:40 +0800

    fix(scripts): remove script 1

--- scripts/random-name.js
+++
@@ -1,8 +0,0 @@
-// Generate a random name combination
-const firstNames = ['Alex', 'Jordan', 'Casey', 'Morgan', 'Riley', 'Avery', 'Quinn', 'Sage', 'Rowan', 'Blake'];
-const lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis', 'Rodriguez', 'Martinez'];
-
-const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-
-console.log(`${randomFirstName} ${randomLastName}`);
