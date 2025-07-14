0af5b76b67d60385af6a7cb7e6eff0c35569e8cc
Lucien Zera <lucienzera26@gmail.com>
2025-07-14 11:55:46 +0800

    build: add expo prebuild file

---
+++ .expo-prebuild/expoPrebuild.ts
@@ -0,0 +1,8 @@
+
+export namespace ExpoPrebuild {
+    // This value is being used directly in `eas.json`, do update that value if this changes.
+    export const APPLE_TEAM_ID = 'PG26Q9276V';
+    export const BUILD_VERSION = '1.13.0';
+    export const IOS_DEPLOYMENT_TARGET = '15.1';
+  
+}
