0700f1046714ea0c6ecdf6ec8ced98c7377998cd
Lucienzera <30294713+Lucienzera@users.noreply.github.com>
2025-07-14 08:38:29 +0000

    chore(build): pltfe bump BUILD_VERSION to v1.26.0

--- .expo-prebuild/expoPrebuild.ts
+++ .expo-prebuild/expoPrebuild.ts
@@ -2,7 +2,7 @@
 export namespace ExpoPrebuild {
     // This value is being used directly in `eas.json`, do update that value if this changes.
     export const APPLE_TEAM_ID = 'PG26Q9276V';
-    export const BUILD_VERSION = '1.25.4';
+    export const BUILD_VERSION = '1.26.0';
     export const IOS_DEPLOYMENT_TARGET = '15.1';
   
 }
